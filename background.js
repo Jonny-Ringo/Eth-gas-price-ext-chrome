5b0529cb76c0699365ede374d29fac5c95de027f
JonnyRingo <92886988+Jonny-Ringo@users.noreply.github.com>
2023-09-02 15:39:42 -0700

    Add files via upload

---
+++ background.js
@@ -0,0 +1,57 @@
+const apiKey = "GWQHH2ZGFJC1PY1BFC6KEV3XK8RSWQTW48";
+
+async function fetchGasPrice() {
+  try {
+    const response = await fetch(`https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=${apiKey}`);
+    const data = await response.json();
+    const prices = data.result;
+
+    const gasPriceMediumGwei = prices.ProposeGasPrice / 1;
+
+    // Prepare icon paths
+    let iconPaths = {
+      "16": "16x16.png",
+      "48": "48x48.png",
+      "128": "128x128.png"
+    };
+
+    // Change icon paths based on gas price condition
+    if (gasPriceMediumGwei < 20) {
+      iconPaths = {
+        "16": "16x16green.png"
+      };
+    }
+
+    // Set icon
+    chrome.action.setIcon({ path: iconPaths });
+  } catch (error) {
+    console.error("Error fetching gas price:", error);
+  }
+}
+
+// Update the badge and icon immediately upon extension load
+fetchGasPrice();
+
+// Schedule a background fetch task for periodic updates
+setInterval(fetchGasPrice, 15000); // Update every 15 seconds
+
+// Add a listener for tab activation
+chrome.tabs.onActivated.addListener((activeInfo) => {
+  // Fetch gas price when a tab is activated
+  fetchGasPrice();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ popup.js
@@ -0,0 +1,38 @@
+const apiKey = "GWQHH2ZGFJC1PY1BFC6KEV3XK8RSWQTW48";
+async function fetchGasPrice() {
+  try {
+    const response = await fetch(`https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=${apiKey}`);
+    const data = await response.json();
+    console.log("Gas Price Data:", data);
+    const prices = data.result;
+
+    // Display medium gas price
+    const gasPriceElement = document.getElementById('gas-price');
+    const gasPriceMedium = prices.ProposeGasPrice / 1;
+    const formattedGasPrice = parseFloat(gasPriceMedium.toFixed(1));
+    gasPriceElement.textContent = `${formattedGasPrice} Gwei`;
+
+    // Change the icon based on gas price
+    if (formattedGasPrice < 20) {
+      chrome.action.setIcon({ path: '16x16green.png' });
+    } else {
+      chrome.action.setIcon({ path: '16x16.png' });
+    }
+  } catch (error) {
+    console.error("Error fetching gas price:", error);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+  console.log("Popup loaded");
+  await fetchGasPrice();
+
+  // Set up interval to update gas price every 15 seconds
+  setInterval(fetchGasPrice, 15000);
+});
+
+
+
+
+
+  
